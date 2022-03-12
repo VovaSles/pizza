f677beeba72ffd6006ce4539b642b012948870bb
VovaSles <vovaswebdev@gmail.com>
2022-03-12 14:29:29 +0200

    basic layout

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,21 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Footer from './componentr/Footer'
+import Layout from './componentr/Layout'
+import Navbar from './componentr/Navbar'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Navbar/>
       <Head>
         <title>Pizza in Haifa</title>
         <meta name="description" content="Best pizza in Haifa" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Hime Page</h1>
+      <Layout/>
+      <Footer/>
     </div>
   )
 }
