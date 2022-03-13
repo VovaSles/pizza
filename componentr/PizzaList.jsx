fb709e53b6cb84995dd64b69802dab5ce91ceda7
VovaSles <vovaswebdev@gmail.com>
2022-03-13 09:12:48 +0200

    slider

---
+++ componentr/PizzaList.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '../styles/PizzaList.module.css'
+
+function PizzaList() {
+  return (
+    <div>PizzaList</div>
+  )
+}
+
+export default PizzaList
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,17 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Footer from './componentr/Footer'
-import Layout from './componentr/Layout'
-import Navbar from './componentr/Navbar'
+import Featured from '../componentr/Featured'
+
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Navbar/>
       <Head>
         <title>Pizza in Haifa</title>
         <meta name="description" content="Best pizza in Haifa" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Layout/>
-      <Footer/>
+      <Featured/>
     </div>
   )
 }
